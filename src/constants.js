e7dfdcde63522c6d580a9e397a8a30d04ce4c00e
htoooth <ht.anglenx@gmail.com>
2017-07-28 17:36:58 +0800

    fix

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const moment = require('moment')
 const todayFile = path.resolve('./md', moment().format('YYYY-MM-DD') + '.md')
-const readmeFile = path.resolve('./readme.md')
+const readmeFile = path.resolve('./README.md')
 
 module.exports = {
   url: 'https://news.ycombinator.com/',
