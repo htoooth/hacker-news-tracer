2a8e4c223bed806f26d49309d614a54f0e82255e
htoooth <ht.anglenx@gmail.com>
2017-07-28 18:04:00 +0800

    fix

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 A tool that gather automatically [hackernews](https://news.ycombinator.com/)'s latest news everyday.
 
 ## List
-### [2017-07-28](./md/2017-07-28.md) 
+
--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const moment = require('moment')
-const todayFile = path.resolve('./md', moment().format('YYYY-MM-DD') + '.md')
-const readmeFile = path.resolve('./README.md')
+const todayFile = path.resolve(__dirname,'../md', moment().format('YYYY-MM-DD') + '.md')
+const readmeFile = path.resolve(__dirname, '../README.md')
 
 module.exports = {
   url: 'https://news.ycombinator.com/',
